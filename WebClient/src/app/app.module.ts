2e6ed9b991fe6c70068db62834d3a618549cb79b
c11andra <c11andra@github.com>
2018-12-30 11:38:41 +0530

    Remove unused sidebar component

--- WebClient/src/app/app.module.ts
+++ WebClient/src/app/app.module.ts
@@ -4,18 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContactSupportComponent } from './contact-support/contact-support.component';
 import { AboutComponent } from './about/about.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AllMaterialModule } from 'src/material-module';
+import { AllMaterialModule } from '../material-module';
 import { SideNavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SidebarComponent,
     ContactSupportComponent,
     AboutComponent,
     SideNavComponent
