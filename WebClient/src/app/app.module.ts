d150f06ab107088577ed2adb6fce85dfbccc3f3b
c11andra <c11andra@github.com>
2019-02-02 23:32:47 +0530

    Add signin form

--- WebClient/src/app/app.module.ts
+++ WebClient/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AllMaterialModule } from '../material-module';
 import { SideNavComponent } from './sidenav/sidenav.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SignInComponent } from './signin/signin.component';
+import { ReactiveFormsModule }    from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { SignInComponent } from './signin/signin.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     AllMaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- WebClient/src/app/signin/signin.component.ts
+++ WebClient/src/app/signin/signin.component.ts
@@ -1,19 +1,90 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 import { SignInService } from '../signin.service';
+
+//import { AlertService, AuthenticationService } from '../_services';
 
 @Component({
   selector: 'app-signin',
-  templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  templateUrl: 'signin.component.html',
+  styleUrls: ['signin.component.scss'],
 })
-export class SignInComponent implements OnDestroy {
-  
-  constructor(private signInService:SignInService) { 
-    this.signInService.setSigningInStatus(true);
-  }
+export class SignInComponent implements OnInit, OnDestroy {
+    loginForm: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
+    hidePass = true;
 
-  ngOnDestroy(): void {
-    this.signInService.setSigningInStatus(false);
-  }
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private signInService:SignInService
+     //  private authenticationService: AuthenticationService,
+      //  private alertService: AlertService
+    ) {
+        signInService.setSigningInStatus(true);
+        // redirect to home if already logged in
+        // if (this.authenticationService.currentUserValue) { 
+        //     this.router.navigate(['/']);
+        // }
+    }
 
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    ngOnDestroy()
+    {
+        this.signInService.setSigningInStatus(false);
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.loginForm.controls; }
+
+    showUsernameError():boolean
+    {
+        if(this.f.username.errors)
+        {
+            return this.submitted && this.f.username.errors.required;
+        }
+        return false;
+    }
+
+    showPasswordError():boolean
+    {
+        if(this.f.password.errors)
+        {
+            return this.submitted && this.f.password.errors.required;
+        }
+        return false;
+    }
+
+    onSubmit() {
+        this.submitted = true;
+        if (this.loginForm.invalid) {
+            return;
+        }
+
+       // this.loading = true;
+        // this.authenticationService.login(this.f.username.value, this.f.password.value)
+        //     .pipe(first())
+        //     .subscribe(
+        //         data => {
+        //             this.router.navigate([this.returnUrl]);
+        //         },
+        //         error => {
+        //             this.alertService.error(error);
+        //             this.loading = false;
+        //         });
+    }
 }
